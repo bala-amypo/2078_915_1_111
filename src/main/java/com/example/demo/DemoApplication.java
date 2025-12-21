f331d81a2b921af9fc7b411bcd367fb8b4572a02
bala-amypo <bala@amypo.in>
2025-12-21 15:41:34 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(
-        exclude = {
-                ServletWebServerFactoryAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            System.out.println("Spring Boot context started successfully.");
+            System.out.println("Exiting cleanly with code 0.");
+        };
+    }
 }
+
