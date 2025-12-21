4e6cf98a4936fd8b1fe62a2dac18b2ec5c42ff8c
bala-amypo <bala@amypo.in>
2025-12-21 15:32:34 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                ServletWebServerFactoryAutoConfiguration.class
+        }
+)
 public class DemoApplication {
 
     public static void main(String[] args) {
