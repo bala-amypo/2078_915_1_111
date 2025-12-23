d5ab2835cdfa54b6b664f793bf30fa4f27834794
bala-amypo <bala@amypo.in>
2025-12-23 03:52:27 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,6 +1,15 @@
 package com.example.demo.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
-    public boolean validate(String token);
-    public String generateToken(String email, String role, String userId, String name);
+
+    public boolean validate(String token) {
+        return true;
+    }
+
+    public String generateToken(String email, String role, String userId, String name) {
+        return "dummy-token";
+    }
 }
