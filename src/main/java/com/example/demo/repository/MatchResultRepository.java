e538b6956b3e936210228e5ed99b9cdf10b96b8d
bala-amypo <bala@amypo.in>
2025-12-27 06:41:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/MatchResultRepository.java
+++ src/main/java/com/example/demo/repository/MatchResultRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.MatchResult;
+import com.example.demo.model.MatchAttemptRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface MatchResultRepository extends JpaRepository<MatchResult, Long> {
-
-    List<MatchResult> findByStudentAIdOrStudentBId(Long a, Long b);
+@Repository
+public interface MatchAttemptRecordRepository extends JpaRepository<MatchAttemptRecord, Long> {
+    List<MatchAttemptRecord> findByInitiatorStudentIdOrCandidateStudentId(Long initiatorId, Long candidateId);
 }
