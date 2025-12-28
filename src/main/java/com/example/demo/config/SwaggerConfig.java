e91afb5183881087161ce74545a1445488b44be1
bala-amypo <bala@amypo.in>
2025-12-28 13:18:26 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,11 +1,19 @@
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+package com.example.demo.config;
 
-@Controller
-public class HomeController {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-    @GetMapping("/")
-    public String redirectToSwagger() {
-        return "redirect:/swagger-ui/index.html";
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Demo API")
+                        .version("1.0")
+                        .description("API documentation for Demo project"));
     }
 }
