b86b40c434750849ffc7f4af509c41fb17eae854
bala-amypo <bala@amypo.in>
2025-12-28 16:50:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/HabitProfileController.java
+++ src/main/java/com/example/demo/controller/HabitProfileController.java
@@ -5,8 +5,10 @@ import com.example.demo.service.HabitProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/habits")
+@RequestMapping("/api/habits")
 public class HabitProfileController {
 
     private final HabitProfileService habitService;
@@ -17,24 +19,18 @@ public class HabitProfileController {
 
     // Create or Update Habit
     @PostMapping
-    public ResponseEntity<HabitProfile> create(@RequestBody HabitProfile habit) {
+    public ResponseEntity<HabitProfile> saveHabit(@RequestBody HabitProfile habit) {
         HabitProfile saved = habitService.createOrUpdateHabit(habit);
         return ResponseEntity.ok(saved);
     }
 
-    // Get Habit By Student ID
+    // Get Habit By Student Id
     @GetMapping("/{studentId}")
-    public ResponseEntity<HabitProfile> getByStudent(@PathVariable long studentId) {
-        return habitService.getHabitByStudent(studentId)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
+    public ResponseEntity<HabitProfile> getHabit(@PathVariable Long studentId) {
 
-    // Get Habit By Habit ID
-    @GetMapping("/id/{id}")
-    public ResponseEntity<HabitProfile> getById(@PathVariable long id) {
-        return habitService.getHabitById(id)
-                .map(ResponseEntity::ok)
+        Optional<HabitProfile> habit = habitService.getHabitByStudent(studentId);
+
+        return habit.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 }
