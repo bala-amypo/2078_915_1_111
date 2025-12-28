1daab4e093316a531a449c90525e0cbdb558c3f5
bala-amypo <bala@amypo.in>
2025-12-28 07:30:21 +0000

    User commit

--- src/main/java/com/example/demo/model/RoomAssignmentRecord.java
+++ src/main/java/com/example/demo/model/RoomAssignmentRecord.java
@@ -1,43 +1,20 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-public class RoomAssignment {
+@Table(name = "room_assignments")
+@Getter
+@Setter
+public class RoomAssignmentRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String studentAId;
-    private String studentBId;
-    private String roomNumber;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getStudentAId() {
-        return studentAId;
-    }
-
-    public void setStudentAId(String studentAId) {
-        this.studentAId = studentAId;
-    }
-
-    public String getStudentBId() {
-        return studentBId;
-    }
-
-    public void setStudentBId(String studentBId) {
-        this.studentBId = studentBId;
-    }
-
-    public String getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
-    }
+    private Long studentAId;
+    private Long studentBId;
+    private String status;
 }
