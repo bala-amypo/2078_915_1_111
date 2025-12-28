e77d04ec1ce813ff4ba4ba45e4bb0f6c19fc3ca2
bala-amypo <bala@amypo.in>
2025-12-28 07:37:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/RoomAssignmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/RoomAssignmentRecordRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.RoomAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomAssignmentRepository extends JpaRepository<RoomAssignment, Long> {
+public interface RoomAssignmentRecordRepository extends JpaRepository<RoomAssignment, Long> {
 }
