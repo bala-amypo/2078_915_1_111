cc1e30c440d624d808a842bf9ecc481cf3be660d
bala-amypo <bala@amypo.in>
2025-12-28 08:10:51 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RoomAssignmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RoomAssignmentServiceImpl.java
@@ -1,15 +1,18 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.RoomAssignment;
+import com.example.demo.model.RoomAssignmentRecord;
 import com.example.demo.model.StudentProfile;
-import com.example.demo.repository.RoomAssignmentRepository;
+import com.example.demo.repository.RoomAssignmentRecordRepository;
 import com.example.demo.repository.StudentProfileRepository;
 import com.example.demo.service.RoomAssignmentService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class RoomAssignmentServiceImpl implements RoomAssignmentService {
+
 
     private final RoomAssignmentRepository roomRepo;
     private final StudentProfileRepository studentRepo;
